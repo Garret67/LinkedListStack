5be4a6fc21ef69debdd7cdc1f05d4d59fe26c18c
67Garret <hal.garcia.daniel.2025@gmail.com>
2024-05-09 01:57:07 +0900

    Mi stack

--- LinkedListStack/Stack.cpp
+++ LinkedListStack/Stack.cpp
@@ -1,1 +1,61 @@
 #include "Stack.h"
+
+void Stack::Push(int val)
+{
+	if (Top)
+	{
+		End->next = new Data(val);
+		End = End->next;
+	}
+	else
+	{
+		Top = new Data(val);
+		End = Top;
+	}
+	listNumElem++;
+}
+
+int Stack::Pop()
+{
+	if (!Empty())
+	{
+		int val = End->value;
+		listNumElem--;
+		delete End;
+
+		if (listNumElem == 0)
+		{
+			Top = nullptr;
+			End = nullptr;
+		}
+		else
+		{
+			Data* cur = Top;
+			for (int i = 0; i < listNumElem - 1; i++)
+			{
+				cur = cur->next;
+			}
+			cur->next = nullptr;
+			End = cur;
+		}
+
+		return val;
+	}
+	return -1;
+}
+
+int Stack::Size() const
+{
+	return listNumElem;
+}
+
+bool Stack::Empty() const
+{
+	if (listNumElem > 0){
+		return false;
+	}
+	else {
+		return true;
+	}
+	
+}
--- LinkedListStack/Stack.h
+++ LinkedListStack/Stack.h
@@ -2,9 +2,59 @@
 
 class Stack
 {
+private:
+	class Data
+	{
+	public:
+		Data(int value)
+			:
+			value(value)
+		{
+		}
+		void Delete(Data* data)
+		{
+			delete data;
+		}
+	public:
+		int value;
+		Data* next = nullptr;
+	};
 public:
+	Stack() = default;
+	Stack(const Stack& other)
+	{
+		*this = other;
+	}
+	~Stack()
+	{
+		if (!Empty())
+		{
+			while (listNumElem > 0)
+			{
+				Pop();
+			}
+		}
+	}
+	Stack& operator=( Stack const& other)
+	{
+		if (!other.Empty())
+		{
+			Data* cur = other.Top;
+			for (int i = 0; i < other.listNumElem; i++)
+			{
+				Push(cur->value);
+				cur = cur->next;
+			}
+		}
+
+		return *this;
+	}
 	void Push( int val );
 	int Pop();
 	int Size() const;
 	bool Empty() const;
+private:
+	Data* Top = nullptr;
+	Data* End = nullptr;
+	int listNumElem = 0;
 };
